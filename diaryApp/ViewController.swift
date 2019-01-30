2e7f2a4128da8c3fecf471733af92b7060939505
iljoo chae <9662964@gmail.com>
2019-01-29 22:47:38 -0500

    Initial Commit

---
+++ diaryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diaryApp
+//
+//  Created by ILJOOCHAE on 2019. 1. 29..
+//  Copyright © 2019년 com.wpiwireless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
